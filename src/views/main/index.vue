bf61592bf7b00609614373a131684fd825f9fea6
LLLxj <253358001@qq.com>
2019-03-09 15:38:45 +0800

    更新main模块

---
+++ src/views/main/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="app-main">
+    <transition name="fade-transform" mode="out-in">
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </transition>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AppMain',
+}
+</script>
+
+<style scoped>
+.app-main {
+  width:100%;
+  position: relative;
+  overflow: hidden;
+}
+</style>
