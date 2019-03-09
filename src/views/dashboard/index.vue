048c40c435798a98690ff1a7f406d1e92af170a7
LLLxj <253358001@qq.com>
2019-03-09 15:38:29 +0800

    更新dashboard模块

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrap">
+    
+    <navPart></navPart>
+    <div class="main_content">
+      <mainPart></mainPart>
+      
+      
+    </div>
+    
+  </div>
+</template>
+
+<script>
+
+  import navPart from '../nav/index'
+  import mainPart from '../main/index'
+
+  export default {
+    name: 'dashboard',
+    data () {
+      return {
+        
+      }
+    },
+    components: {
+      navPart, mainPart
+    }
+  }
+
+</script>
+<style lang="less" scoped>
+  .wrap{
+    position:relative;
+    width:100%;
+    height:200px;
+  }
+  .main_content{
+    width:100%;
+    height:200px;
+  }
+</style>
