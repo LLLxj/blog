f7e647bea6c20252acbafe643fa84ce30a8203ad
LLLxj <253358001@qq.com>
2019-03-09 15:52:59 +0800

    更新路由文件

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,40 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import dashBoard from '@/views/dashboard/index'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-   
+export const constantRouterMap = [
     {
-      path: '/',
-      name: 'home',
-      component: () => import('@/views/home/index'),
-    },
-    {
-      path: '/dashboard',
+      component: dashBoard,
+      path: '',
       name: 'dashboard',
-      component: () => import('@/views/dashboard/index'),
+      // component: () => import('@/views/dashboard/index'),
     },
-    {
-      path: '/contact',
-      name: 'contact',
-      component: () => import('@/views/contact/index'),
-    },
-    
-  ]
+]
+
+const asyncRouterMap = [
+  {
+    component: dashBoard,
+    path: '',
+    name: 'dashboard',
+    // component: () => import('@/views/dashboard/index'),
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: () => import('@/views/home/index'),
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: () => import('@/views/contact/index'),
+  },
+]
+
+// 临时处理路由权限 全部可以访问
+const allRoyterMap = constantRouterMap.concat(asyncRouterMap)
+export default new Router({
+  // mode: 'history', //后端支持可开
+  routes: allRoyterMap
 })
