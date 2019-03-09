891a78b60c6d5e5484ca920c057ddb53af627b88
LLLxj <253358001@qq.com>
2019-03-09 15:39:09 +0800

    更新配置文件

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
+   
     {
       path: '/',
       name: 'home',
       component: () => import('@/views/home/index'),
-    }
-   
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: () => import('@/views/dashboard/index'),
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('@/views/contact/index'),
+    },
+    
   ]
 })
