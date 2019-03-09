596c21065b6a4fda8ab95d1b87d9fef5b73547a6
LLLxj <253358001@qq.com>
2019-03-09 15:38:18 +0800

    更新contact模块

---
+++ src/views/contact/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <navPart></navPart>
+    <div>aaa</div>
+  </div>
+</template>
+
+<script>
+  import navPart from '../nav/index'
+  export default {
+    name: 'contact',
+    data () {
+      return {
+      }
+    },
+    components: {
+      navPart
+    }
+  }
+
+</script>
+<style>
+</style>
