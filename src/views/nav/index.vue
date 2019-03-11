584f134b95c3b7f4f9584dd95b7cbc256f710fa1
LLLxj <253358001@qq.com>
2019-03-11 17:40:47 +0800

    更新导航模块

--- src/views/nav/index.vue
+++ src/views/nav/index.vue
@@ -1,61 +1,46 @@
 <template>
-  
-  <div class="home_wrap" @scroll="handleScroll()">
-    <!-- 导航 -->
-    <div class="main_header">
+  <transition :name="transitionName">
+    <div class="main_header" v-show="visible">
       <el-row :gutter="24" style="margin: 0px 0px">
         <el-col :span="2" class="main_logo">
           <!-- <a href=""><img src="../public/img/hashiqi.png" alt=""></a> -->
           <span class="name-add-color">L</span><span>iu</span><span>Z</span><span>i</span><span>J</span><span>ian</span>
         </el-col>
-        
-        
-        <el-col :span="6" :offset="15" class="main_menu" :routes="routes">
-          <!-- <router-link to="/"> -->
-          <span @click="$router.push({ name: 'home' })" style="border:1px solid red;">home</span>
-          <!-- </router-link> -->
-          <!-- <router-link to="/contact"> -->
+        <el-col :span="6" :offset="15" class="main_menu">
+          <span @click="$router.push({ name: 'home' })">home</span>
           <span @click="$router.push({ name: 'contact' })">contact</span>
-          <!-- </router-link> -->
-          <!-- <div v-for="item in routers" :key="item.name">
-            <router-link :to="'/'+ item.name" :key="item.name">
-              <span>{{item.name}}</span>
-            </router-link>
-          </div> -->
         </el-col>
-            
         <div class="clear"></div>
       </el-row>
     </div>
-
-  </div>
+  </transition>
 </template>
 
 <script>
 export default {
-  mounted () {
-    // window.addEventListener('scroll', this.appScroll);
-  },
-  computed: {
-    routes() {
-      this.routers = this.$router.options.routes
-      console.log(this.routers)
-      return this.$router.options.routes
+  props: {
+    offYset: {
+      type: [Number, String]
     },
   },
-  data () {
+  data() {
     return {
-      routers: [] 
+     
+      transitionName: 'fade',
+      visible: true,
     }
   },
+　watch:{
+　  offYset(curVal,oldVal){
+      if(curVal === 0) {
+        this.visible = false
+      } else {
+        this.visible = true
+      }
+　　}　　　　　
+　},
   method: {
-    handleScroll () {
-      let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
-      console.log(this.scrollTop)
-    },
-    toRoute () {
-      console.log(1)
-    }
+    
   }
 }
 
@@ -79,11 +64,9 @@ export default {
     text-decoration: none;
     color:black;
   }
-  .home_wrap{
-    background: #141414;
-  }
   .main_header{
     position:fixed;
+    background: #141414;
     top:0;
     left:0;
     width:100%;
@@ -112,5 +95,15 @@ export default {
       }
     }
   }
+   .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 1s;
+  }
+
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0
+  }
+
 </style>
 
