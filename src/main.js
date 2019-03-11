4e4a1eb9bdb250a3967e4d4972d57a0873805dcc
LLLxj <253358001@qq.com>
2019-03-11 17:38:47 +0800

    更新配置文件

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import '@/icons'
 // import '@/styles/transition.scss'
 
 Vue.config.productionTip = false
